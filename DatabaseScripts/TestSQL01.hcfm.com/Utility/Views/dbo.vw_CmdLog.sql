8473764cfb1cf74649b0b2657e66efdd0cd9beec
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-12-22 14:16:34 -0500

    gv

--- DatabaseScripts/TestSQL01.hcfm.com/Utility/Views/dbo.vw_CmdLog.sql
+++ DatabaseScripts/TestSQL01.hcfm.com/Utility/Views/dbo.vw_CmdLog.sql
@@ -1,18 +1,21 @@
-/* CreateDate: 12/20/2021 08:07:31.800 , ModifyDate: 12/20/2021 08:07:31.800 */
+/* CreateDate: 12/20/2021 08:07:31.800 , ModifyDate: 12/22/2021 13:51:35.760 */
 GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-Create view vw_CmdLog as
-SELECT [ID]
-      ,[DatabaseName]
-      ,[IndexName]
-      ,[StatisticsName]
-      ,[ExtendedInfo]
-      ,[Command]
+CREATE view [dbo].[vw_CmdLog] as
+SELECT top 100 percent [ID]
       ,[StartTime]
       ,[EndTime]
+      ,[DatabaseName]
+      ,[CommandType]
+	  ,[SchemaName]
+	  ,[ObjectName]
+	  ,[IndexName]
+	  ,[StatisticsName]
+      --,[ExtendedInfo]
   FROM [dbo].[CommandLog]
   WHERE [StartTime] >  (getdate()-1)
+  order by ID desc
 GO
