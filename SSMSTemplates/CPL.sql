ddfaa3583ed6dfb86efe3674ea5e342a5571b259
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-01-19 13:53:35 -0500

    gv

---
+++ SSMSTemplates/CPL.sql
@@ -0,0 +1,73 @@
+SELECT *
+FROM [sys].[columns]
+WHERE [object_id] = OBJECT_ID('VWFactMarketingBudget') AND [name] IN ('Leads', 'NetSpend','AgencyName')
+ORDER BY [name] ;
+
+SELECT *
+FROM [sys].[columns]
+WHERE [object_id] = OBJECT_ID('VWLead') AND [name] IN ('Leads', 'NetSpend','AgencyName')
+ORDER BY [name] ;
+
+SELECT *
+FROM [sys].[columns]
+WHERE [object_id] = OBJECT_ID('VWMarketingActivity') AND [name] IN ('Leads', 'NetSpend','AgencyName')
+ORDER BY [name] ;
+
+
+DROP TABLE [#Leads]
+DROP TABLE [#NetSpend]
+DROP TABLE [#Step2]
+
+SELECT
+    [AgencyName]
+  , CAST([LeadCreatedDateEST] AS DATE) AS [dt]
+  , COUNT(1) AS [Cnt]
+INTO [#Leads]
+FROM [dbo].[VWLead]
+GROUP BY CAST([LeadCreatedDateEST] AS DATE)
+       , [AgencyName] ;
+
+SELECT
+    [AgencyName]
+  , CAST([MarketingActivityDateEST] AS DATE) AS [Dt]
+  , SUM([NetSpend]) AS [NetSpend]
+  , COUNT(1) AS [Cnt]
+INTO [#NetSpend]
+FROM [dbo].[VWMarketingActivity]
+GROUP BY CAST([MarketingActivityDateEST] AS DATE)
+       , [AgencyName] ;
+
+
+
+SELECT
+    [k].[Dt]
+  --, [k].[AgencyName]
+  , SUM([k].[NetSpend]) AS [NetSpend]
+  , SUM(CASE WHEN [k].[AgencyName] = 'MediaPoint' THEN 200 * [k].[Leads] WHEN [k].[AgencyName] = 'Venator' THEN 125 * [k].[Leads] ELSE [k].[NetSpend] END) AS [NetSpend2]
+  
+  , SUM([k].[SpendCnt]) AS [SpendCnt]
+  , SUM([k].[Leads]) AS [Leads]
+  , CAST(SUM([k].[NetSpend]) / SUM([k].[Leads]) AS MONEY) AS [SpendPerLead]
+  ,CAST(SUM(CASE WHEN [k].[AgencyName] = 'MediaPoint' THEN 200 * [k].[Leads] WHEN [k].[AgencyName] = 'Venator' THEN 125 * [k].[Leads] ELSE [k].[NetSpend] END) / SUM([k].[Leads]) AS money) AS [SpendPerLead2]
+INTO [#Step2]
+FROM( SELECT
+          [Dt]
+        , [AgencyName]
+        , [NetSpend]
+        , [Cnt] AS [SpendCnt]
+        , NULL AS [Leads]
+      FROM [#NetSpend]
+      UNION ALL
+      SELECT
+          [dt]
+        , [AgencyName]
+        , NULL AS [NetSpend]
+        , 0 AS [SpendCnt]
+        , [Cnt] AS [Leads]
+      FROM [#Leads] ) AS [k]
+GROUP BY [k].[Dt]
+--       , [k].[AgencyName] ;
+
+
+
+SELECT * FROM [#Step2] ORDER BY [Dt] Desc
