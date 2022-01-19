1ef67e6449fb366f528a0b04847793eb28cfa1fb
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-01-19 13:58:31 -0500

    gv

--- SSMSTemplates/CPL.sql
+++ SSMSTemplates/CPL.sql
@@ -1,22 +1,26 @@
 SELECT *
 FROM [sys].[columns]
-WHERE [object_id] = OBJECT_ID('VWFactMarketingBudget') AND [name] IN ('Leads', 'NetSpend','AgencyName')
+WHERE [object_id] = OBJECT_ID('VWFactMarketingBudget') AND [name] IN ('Leads', 'NetSpend', 'AgencyName')
 ORDER BY [name] ;
 
 SELECT *
 FROM [sys].[columns]
-WHERE [object_id] = OBJECT_ID('VWLead') AND [name] IN ('Leads', 'NetSpend','AgencyName')
+WHERE [object_id] = OBJECT_ID('VWLead') AND [name] IN ('Leads', 'NetSpend', 'AgencyName')
 ORDER BY [name] ;
 
 SELECT *
 FROM [sys].[columns]
-WHERE [object_id] = OBJECT_ID('VWMarketingActivity') AND [name] IN ('Leads', 'NetSpend','AgencyName')
+WHERE [object_id] = OBJECT_ID('VWMarketingActivity') AND [name] IN ('Leads', 'NetSpend', 'AgencyName')
 ORDER BY [name] ;
 
+IF OBJECT_ID('[tempdb]..[#Leads]') IS NOT NULL
+    DROP TABLE [#Leads] ;
 
-DROP TABLE [#Leads]
-DROP TABLE [#NetSpend]
-DROP TABLE [#Step2]
+IF OBJECT_ID('[tempdb]..[#NetSpend]') IS NOT NULL
+    DROP TABLE [#NetSpend] ;
+
+IF OBJECT_ID('[tempdb]..[#Step2]') IS NOT NULL
+    DROP TABLE [#Step2] ;
 
 SELECT
     [AgencyName]
@@ -24,6 +28,7 @@ SELECT
   , COUNT(1) AS [Cnt]
 INTO [#Leads]
 FROM [dbo].[VWLead]
+WHERE [Isvalid] = 1 AND [IsDeleted] = 0
 GROUP BY CAST([LeadCreatedDateEST] AS DATE)
        , [AgencyName] ;
 
@@ -34,21 +39,19 @@ SELECT
   , COUNT(1) AS [Cnt]
 INTO [#NetSpend]
 FROM [dbo].[VWMarketingActivity]
+WHERE ISNULL([Company], '') <> 'Hans Wiemann'
 GROUP BY CAST([MarketingActivityDateEST] AS DATE)
        , [AgencyName] ;
-
-
 
 SELECT
     [k].[Dt]
   --, [k].[AgencyName]
   , SUM([k].[NetSpend]) AS [NetSpend]
   , SUM(CASE WHEN [k].[AgencyName] = 'MediaPoint' THEN 200 * [k].[Leads] WHEN [k].[AgencyName] = 'Venator' THEN 125 * [k].[Leads] ELSE [k].[NetSpend] END) AS [NetSpend2]
-  
   , SUM([k].[SpendCnt]) AS [SpendCnt]
   , SUM([k].[Leads]) AS [Leads]
   , CAST(SUM([k].[NetSpend]) / SUM([k].[Leads]) AS MONEY) AS [SpendPerLead]
-  ,CAST(SUM(CASE WHEN [k].[AgencyName] = 'MediaPoint' THEN 200 * [k].[Leads] WHEN [k].[AgencyName] = 'Venator' THEN 125 * [k].[Leads] ELSE [k].[NetSpend] END) / SUM([k].[Leads]) AS money) AS [SpendPerLead2]
+  , CAST(SUM(CASE WHEN [k].[AgencyName] = 'MediaPoint' THEN 200 * [k].[Leads] WHEN [k].[AgencyName] = 'Venator' THEN 125 * [k].[Leads] ELSE [k].[NetSpend] END) / SUM([k].[Leads]) AS MONEY) AS [SpendPerLead2]
 INTO [#Step2]
 FROM( SELECT
           [Dt]
@@ -65,9 +68,9 @@ FROM( SELECT
         , 0 AS [SpendCnt]
         , [Cnt] AS [Leads]
       FROM [#Leads] ) AS [k]
-GROUP BY [k].[Dt]
+GROUP BY [k].[Dt] ;
+
 --       , [k].[AgencyName] ;
-
-
-
-SELECT * FROM [#Step2] ORDER BY [Dt] Desc
+SELECT *
+FROM [#Step2]
+ORDER BY [Dt] DESC ;
