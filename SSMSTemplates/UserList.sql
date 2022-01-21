ca1733a6eb9c8a0cfc2e7c48f41919ef54a63bd7
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-01-21 11:08:23 -0500

    gv

---
+++ SSMSTemplates/UserList.sql
@@ -0,0 +1,7 @@
+SELECT
+	[Company], [Email], [User]
+FROM (VALUES
+('Raagvitech', 'vbachu@hairclub.com', 'HCFM\dbo'),
+('Raagvitech', 'tvijay@hairclub.com', 'HCFM\dbo'),
+('Raagvitech', 'ksumit@hairclub.com', 'HCFM\dbo'))
+[vdata] ([Company], [Email], [User])
