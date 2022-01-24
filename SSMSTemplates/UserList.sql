5d920614b01a1bacbf686643fbe459c8acc87aaa
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-01-24 10:47:27 -0500

    gv

--- SSMSTemplates/UserList.sql
+++ SSMSTemplates/UserList.sql
@@ -1,7 +1,17 @@
 SELECT
 	[Company], [Email], [User]
 FROM (VALUES
-('Raagvitech', 'vbachu@hairclub.com', 'HCFM\dbo'),
-('Raagvitech', 'tvijay@hairclub.com', 'HCFM\dbo'),
-('Raagvitech', 'ksumit@hairclub.com', 'HCFM\dbo'))
+('Raagvitech', 'vbachu@hairclub.com', 'HCFM\vbachu'),
+('Raagvitech', 'tvijay@hairclub.com', 'HCFM\tvijay'),
+('Raagvitech', 'ksumit@hairclub.com', 'HCFM\ksumit'))
 [vdata] ([Company], [Email], [User])
+
+SELECT
+	'ShopDev' AS Company, [Email], NULL AS [User]
+FROM (VALUES
+('adnan.mehboob@shopdev.co'),
+('amana.khan@shopdev.co'),
+('kashif.irshad@shopdev.co'),
+('kashif.irshad@shopdev.co '),
+('raheel@shopdev.co'))
+[vdata] ([Email])
