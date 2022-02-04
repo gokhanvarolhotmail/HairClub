446f7d5328f740c8d72fe6e1884bc9834c132aab
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-02-04 09:52:40 -0500

    gv

--- SSMSTemplates/rptHairOrderQuantitybyClient_GVAROL.sql
+++ SSMSTemplates/rptHairOrderQuantitybyClient_GVAROL.sql
@@ -415,3 +415,35 @@ GO
 
 EXEC [dbo].[rptHairOrderQuantitybyClient_GVAROL] 201, '0' ;
 
+GO
+RETURN
+SELECT
+    QUOTENAME([s].[name]) + '.' + QUOTENAME([t].[name]) AS [FQN]
+  , [s].[name] AS [SchemaName]
+  , [t].[name] AS [TableName]
+  , [c].[name] AS [ColumnName]
+  , [y].[name] AS [DataType]
+  , CAST(CASE WHEN [y].[name] = 'timestamp' THEN 'rowversion'
+             WHEN [y].[name] IN ('char', 'varchar') THEN CONCAT([y].[name], '(', CASE WHEN [c].[max_length] = -1 THEN 'MAX' ELSE CAST([c].[max_length] AS VARCHAR)END, ')', CASE WHEN [c].[collation_name] <> [d].[collation_name] THEN CONCAT(' COLLATE ', [c].[collation_name])ELSE '' END)
+             WHEN [y].[name] IN ('nchar', 'nvarchar') THEN CONCAT([y].[name], '(', CASE WHEN [c].[max_length] = -1 THEN 'MAX' ELSE CAST([c].[max_length] / 2 AS VARCHAR)END, ')', CASE WHEN [c].[collation_name] <> [d].[collation_name] THEN CONCAT(' COLLATE ', [c].[collation_name])ELSE '' END)
+             WHEN [y].[name] IN ('binary', 'varbinary') THEN CONCAT([y].[name], '(', CASE WHEN [c].[max_length] = -1 THEN 'MAX' ELSE CAST([c].[max_length] AS VARCHAR)END, ')')
+             WHEN [y].[name] IN ('bigint', 'int', 'smallint', 'tinyint') THEN [y].[name]
+             WHEN [y].[name] IN ('datetime2', 'time', 'datetimeoffset') THEN CONCAT([y].[name], '(', [c].[scale], ')')
+             WHEN [y].[name] IN ('numeric', 'decimal') THEN CONCAT([y].[name], '(', [c].[precision], ', ', [c].[scale], ')')
+             ELSE [y].[name]
+         END AS VARCHAR(256)) AS [ColumnDef]
+  , [c].[is_nullable]
+FROM [sys].[tables] AS [t]
+INNER JOIN [sys].[schemas] AS [s] ON [s].[schema_id] = [t].[schema_id]
+INNER JOIN [sys].[columns] AS [c] ON [c].[object_id] = [t].[object_id]
+INNER JOIN [sys].[types] AS [y] ON [y].[user_type_id] = [c].[user_type_id]
+LEFT JOIN [sys].[databases] AS [d] ON [d].[name] = DB_NAME()
+WHERE [c].[name] LIKE REPLACE('Last Application Date', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Scheduled Next App Date', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Order Avail for Next App', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Oldest Order Placed Date', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Oldest Order Placed Due Date', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Newest Order System Type', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Remaining Qty to Order', ' ', '%')
+   OR [c].[name] LIKE REPLACE('Priority Hair Needed', ' ', '%') ;
+GO
