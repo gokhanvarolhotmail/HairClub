6f17552fa5ea63fcf03a7031a04970598d40ed1b
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-03-03 14:23:41 -0500

    gv

--- SSMSTemplates/SalesForcePipelines/GetMaxDates.sql
+++ SSMSTemplates/SalesForcePipelines/GetMaxDates.sql
@@ -1,3 +1,20 @@
+IF OBJECT_ID('[tempdb]..[#Tables]') IS NOT NULL
+    DROP TABLE [#Tables] ;
+
+SELECT
+    CONCAT(QUOTENAME([s].[name]), '.', QUOTENAME([t].[name])) AS [FQN]
+  , [s].[name] AS [SchemaName]
+  , [t].[name] AS [TableName]
+  , COUNT(1) AS [Cnt]
+INTO [#Tables]
+FROM [sys].[tables] AS [t]
+INNER JOIN [sys].[schemas] AS [s] ON [s].[schema_id] = [t].[schema_id]
+INNER JOIN [sys].[columns] AS [c] ON [c].[object_id] = [t].[object_id]
+WHERE [s].[name] = 'SF' AND [c].[name] IN ('CreatedDate', 'LastModifiedDate')
+GROUP BY [s].[name]
+       , [t].[name]
+HAVING COUNT(1) = 2 ;
+
 IF OBJECT_ID('[tempdb]..[#Dates]') IS NOT NULL
     DROP TABLE [#Dates] ;
 
@@ -12,17 +29,7 @@ SELECT
 FROM( SELECT MAX([CreatedDate]) AS [LastDate] FROM [SF].[Account] UNION ALL SELECT MAX([LastModifiedDate]) AS [LastDate] FROM ', [k].[FQN], ' ) AS [k]
 
 ')                  AS [SQL]
-                FROM( SELECT
-                          CONCAT(QUOTENAME([s].[name]), '.', QUOTENAME([t].[name])) AS [FQN]
-                        , [s].[name] AS [SchemaName]
-                        , [t].[name] AS [TableName]
-                      FROM [sys].[tables] AS [t]
-                      INNER JOIN [sys].[schemas] AS [s] ON [s].[schema_id] = [t].[schema_id]
-                      INNER JOIN [sys].[columns] AS [c] ON [c].[object_id] = [t].[object_id]
-                      WHERE [s].[name] = 'SF' AND [c].[name] IN ('CreatedDate', 'LastModifiedDate')
-                      GROUP BY [s].[name]
-                             , [t].[name]
-                      HAVING COUNT(1) = 2 ) AS [k]
+                FROM( SELECT [FQN], [SchemaName], [TableName], [Cnt] FROM [#Tables] ) AS [k]
               FOR XML PATH(N''), TYPE ).[value](N'.', N'NVARCHAR(MAX)') ;
 
 EXEC( @SQL ) ;
