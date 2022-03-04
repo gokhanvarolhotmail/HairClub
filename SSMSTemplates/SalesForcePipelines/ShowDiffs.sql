34384472246cdf59b4498e84b7686d7c2736b3d0
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-03-04 10:52:51 -0500

    gv

--- SSMSTemplates/SalesForcePipelines/ShowDiffs.sql
+++ SSMSTemplates/SalesForcePipelines/ShowDiffs.sql
@@ -22,11 +22,13 @@ IF OBJECT_ID('tempdb..#temp1') IS NULL
 ELSE
     BEGIN
         SELECT
-            ISNULL([a].[FQN], [b].[FQN]) AS [name]
+            DATEDIFF(SECOND, [tb].[create_date], GETDATE()) AS [DurationSec]
+          , ISNULL([a].[FQN], [b].[FQN]) AS [name]
           , [a].[rows] AS [PreviousRows]
           , [b].[rows] AS [CurrentRows]
-          , ISNULL([b].[rows], 0) - ISNULL([a].[rows], 0) AS [diff]
+          , ISNULL([b].[rows], 0) - ISNULL([a].[rows], 0) AS [RowsDiff]
         FROM [#temp1] AS [a]
+        INNER JOIN [tempdb].[sys].[tables] AS [tb] ON [tb].[object_id] = OBJECT_ID('[tempdb]..[#temp1]')
         FULL OUTER JOIN [#temp2] AS [b] ON [a].[FQN] = [b].[FQN]
         ORDER BY ABS(ISNULL([b].[rows], 0) - ISNULL([a].[rows], 0)) DESC
                , ISNULL([a].[FQN], [b].[FQN]) ;
