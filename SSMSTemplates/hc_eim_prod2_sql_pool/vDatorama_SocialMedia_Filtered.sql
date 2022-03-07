7ad4f79c4debdc63fc3e3a4f5ebf39ca6fe1e6e5
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-03-07 11:08:19 -0500

    gv

--- SSMSTemplates/hc_eim_prod2_sql_pool/vDatorama_SocialMedia_Filtered.sql
+++ SSMSTemplates/hc_eim_prod2_sql_pool/vDatorama_SocialMedia_Filtered.sql
@@ -10,6 +10,18 @@ SELECT
   , [d].[FilePath]
   , [d].[DWH_LoadDate]
   , [d].[MediaSpend]
-FROM [ODS].[Datorama_SocialMedia] AS [d]
-WHERE [d].[DWH_LoadDate] > DATEADD(DAY, -3, GETUTCDATE()) AND [d].[DWH_LoadDate] <= GETUTCDATE() ;
+FROM( SELECT
+          [d].[DataStream]
+        , [d].[CRMDay]
+        , [d].[CampaignAdvertiserID]
+        , [d].[CampaignAdvertaiser]
+        , [d].[MediaCost]
+        , [d].[Clicks]
+        , [d].[FilePath]
+        , [d].[DWH_LoadDate]
+        , [d].[MediaSpend]
+        , ROW_NUMBER() OVER ( PARTITION BY [d].[DataStream], [d].[CRMDay] ORDER BY [d].[DWH_LoadDate] DESC ) AS [Row]
+      FROM [ODS].[Datorama_SocialMedia] AS [d]
+      WHERE [d].[DWH_LoadDate] > DATEADD(DAY, -3, GETUTCDATE()) AND [d].[DWH_LoadDate] <= GETUTCDATE()) AS [d]
+WHERE [d].[Row] = 1 ;
 GO
