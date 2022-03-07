4b9107a28dd86eaa603fb402e379f4dce873cf53
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2022-03-07 10:11:30 -0500

    gv

---
+++ SSMSTemplates/hc_eim_prod2_sql_pool/vDatorama_SocialMedia_Filtered.sql
@@ -0,0 +1,15 @@
+ALTER VIEW [ODS].[vDatorama_SocialMedia_Filtered]
+AS
+SELECT
+    [d].[DataStream]
+  , [d].[CRMDay]
+  , [d].[CampaignAdvertiserID]
+  , [d].[CampaignAdvertaiser]
+  , [d].[MediaCost]
+  , [d].[Clicks]
+  , [d].[FilePath]
+  , [d].[DWH_LoadDate]
+  , [d].[MediaSpend]
+FROM [ODS].[Datorama_SocialMedia] AS [d]
+WHERE [d].[DWH_LoadDate] > DATEADD(DAY, -60, GETUTCDATE()) AND [d].[DWH_LoadDate] <= GETUTCDATE() ;
+GO
